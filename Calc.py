a2b4d4da3570c34fc49cb4a51aefd54eaa3a45f9
Rulons <klaincordey@gmail.com>
2024-11-08 13:57:06 +0000

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter the first number:")) #the user is prompted to enter two numbers
+num2 = float(input("Enter the second number:")) 
+
+sum_result = num1 + num2 # add both the users numbers together
+difference_result = num1 - num2 #takes the first input - the second input
+product_result = num1 * num2 #times the two results together
+quotient_result = num1 / num2 # divides both the numbers
+
+print (sum_result)
+print (difference_result)
+print (product_result)
+print (quotient_result)
